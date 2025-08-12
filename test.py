c401186205421d9d298c51d657717f0d72fa9513
galactimas <galactimas@gmail.com>
2025-08-12 22:01:20 +0530

    ADDED SOMETHING<

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 def add(a,b):
     return a+b
-def subtract(a,b)
+def subtract(a,b):
     return a-b
+
+
+
+def primechecker(n):
+    if n%2 != 0:
+        print("prime")
+    else:
+        print("not") 
+        def primechecker(n):
+    if n%2 != 0:
+        print("prime")
+    else:
+        print("not")    
